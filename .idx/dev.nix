f8689fb9dca0c09c2d928e2f416f769e4aa5ac6a
calebliskey51 <calebliskey51@gmail.com>
2026-03-11 03:31:29 +0000

    Initial commit

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -5,11 +5,7 @@
   channel = "stable-24.05"; # or "unstable"
   # Use https://search.nixos.org/packages to find packages
   packages = [
-    # pkgs.go
-    # pkgs.python311
-    # pkgs.python311Packages.pip
-    # pkgs.nodejs_20
-    # pkgs.nodePackages.nodemon
+    pkgs.python311
   ];
   # Sets environment variables in the workspace
   env = {};
@@ -23,16 +19,10 @@
     previews = {
       enable = true;
       previews = {
-        # web = {
-        #   # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
-        #   # and show it in IDX's web preview panel
-        #   command = ["npm" "run" "dev"];
-        #   manager = "web";
-        #   env = {
-        #     # Environment variables to set for your server
-        #     PORT = "$PORT";
-        #   };
-        # };
+        web = {
+          command = ["python3" "-m" "http.server" "$PORT"];
+          manager = "web";
+        };
       };
     };
     # Workspace lifecycle hooks
@@ -42,7 +32,7 @@
         # Example: install JS dependencies from NPM
         # npm-install = "npm install";
         # Open editors for the following files by default, if they exist:
-        default.openFiles = [ ".idx/dev.nix" "README.md" ];
+        default.openFiles = [ "index.html" ".idx/dev.nix" ];
       };
       # Runs when the workspace is (re)started
       onStart = {
